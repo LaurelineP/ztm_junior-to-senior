8094d62fba3ce276bb08c8c87dc4980ac48e1aab
Laureline <>
2023-08-31 00:44:00 +0200

    [ performances ] code splitting application

--- practices/06_performance_code-splitting-exercise/src/App/App.js
+++ practices/06_performance_code-splitting-exercise/src/App/App.js
@@ -1,38 +1,63 @@
 
-import { useState } from 'react';
-import { NavigationTabs } from '../components/NavigationTabs/NavigationTabs';
-import { Page } from '../components/Page/Page';
+import { useState, useEffect } from 'react';
+import { Loader } from '../components/Loader/Loader';
 import './App.css';
 
 function App() {
 	const [ pageId, setPageId ] = useState('tab1');
+	const [ tabList, setTabList ] = useState([]);
+	const [ DynamicNavigationTabs, setDynamicNavigationTabsComponent ] = useState(null);
+	const [ DynamicPage, setDynamicPageComponent ] = useState(null);
+	
 
-	const tabs = [
+	const _tabs = [
 		{ title: 'Tab 1', id: 'tab1'},
 		{ title: 'Tab 2', id: 'tab2'},
 		{ title: 'Tab 3', id: 'tab3'}
 	]
 
-  	const pageRawContent  = tabs?.find(x => x.id === pageId)?.title;
+	const pageRawContent  = `${'Dynamic'} ${tabList?.find(x => x.id === pageId)?.title}`;
 
 	const onTabSelection = (e) => {
 		setPageId(e.target.id);
 	}
 
-	return(
+	// Splitting: Dynamic import code to be loaded after 1sec
+	useEffect(() => {
+		// content to render on code splitting
+		setTimeout(() => {
+			Promise.all([
+				_tabs,
+				import('../components/NavigationTabs/NavigationTabs.js'),
+				import('../components/Page/Page.js')
+			]).then( data => {
+					setTabList(data[0]);
+					setDynamicNavigationTabsComponent(data[1]);
+					setDynamicPageComponent(data[2]);
+			}).catch(console.error);
+		}, 5_000)
+	}, [])
+
+
+	return (
 		<div className="App">
 			<header className="App-header">
 				<p>Code Splitting and Performance!</p>
 			</header>
 
-			<div className="fade-in">
-				<NavigationTabs list={tabs} onTabSelection={onTabSelection}/>
-				<div className="App-content">
-					<Page>
-					<h1>{pageRawContent}</h1>
-					</Page>
+			{/* Enables the Code Splitting experience */}
+			{!!tabList?.length ? (
+				<div className="fade-in">
+					<DynamicNavigationTabs.NavigationTabs
+						list={tabList}
+						onTabSelection={onTabSelection}/>
+					<div className="App-content">
+						<DynamicPage.Page>
+							<h1>{pageRawContent}</h1>
+						</DynamicPage.Page>
+					</div>
 				</div>
-			</div>
+			): <Loader hasMessage/> }
 		</div>
 	);
 }
