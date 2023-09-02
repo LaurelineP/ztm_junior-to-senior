41ccc422a09a62d195d969c5a3c49bcfbcabff18
Laureline Paris <32878345+LaurelineP@users.noreply.github.com>
2023-09-02 22:19:34 +0200

    Revert "[ performances ] code splitting application"

--- practices/06_performance_code-splitting-exercise/src/App/App.js
+++ practices/06_performance_code-splitting-exercise/src/App/App.js
@@ -1,63 +1,38 @@
 
-import { useState, useEffect } from 'react';
-import { Loader } from '../components/Loader/Loader';
+import { useState } from 'react';
+import { NavigationTabs } from '../components/NavigationTabs/NavigationTabs';
+import { Page } from '../components/Page/Page';
 import './App.css';
 
 function App() {
 	const [ pageId, setPageId ] = useState('tab1');
-	const [ tabList, setTabList ] = useState([]);
-	const [ DynamicNavigationTabs, setDynamicNavigationTabsComponent ] = useState(null);
-	const [ DynamicPage, setDynamicPageComponent ] = useState(null);
-	
 
-	const _tabs = [
+	const tabs = [
 		{ title: 'Tab 1', id: 'tab1'},
 		{ title: 'Tab 2', id: 'tab2'},
 		{ title: 'Tab 3', id: 'tab3'}
 	]
 
-	const pageRawContent  = `${'Dynamic'} ${tabList?.find(x => x.id === pageId)?.title}`;
+  	const pageRawContent  = tabs?.find(x => x.id === pageId)?.title;
 
 	const onTabSelection = (e) => {
 		setPageId(e.target.id);
 	}
 
-	// Splitting: Dynamic import code to be loaded after 1sec
-	useEffect(() => {
-		// content to render on code splitting
-		setTimeout(() => {
-			Promise.all([
-				_tabs,
-				import('../components/NavigationTabs/NavigationTabs.js'),
-				import('../components/Page/Page.js')
-			]).then( data => {
-					setTabList(data[0]);
-					setDynamicNavigationTabsComponent(data[1]);
-					setDynamicPageComponent(data[2]);
-			}).catch(console.error);
-		}, 5_000)
-	}, [])
-
-
-	return (
+	return(
 		<div className="App">
 			<header className="App-header">
 				<p>Code Splitting and Performance!</p>
 			</header>
 
-			{/* Enables the Code Splitting experience */}
-			{!!tabList?.length ? (
-				<div className="fade-in">
-					<DynamicNavigationTabs.NavigationTabs
-						list={tabList}
-						onTabSelection={onTabSelection}/>
-					<div className="App-content">
-						<DynamicPage.Page>
-							<h1>{pageRawContent}</h1>
-						</DynamicPage.Page>
-					</div>
+			<div className="fade-in">
+				<NavigationTabs list={tabs} onTabSelection={onTabSelection}/>
+				<div className="App-content">
+					<Page>
+					<h1>{pageRawContent}</h1>
+					</Page>
 				</div>
-			): <Loader hasMessage/> }
+			</div>
 		</div>
 	);
 }
--- practices/06_performance_code-splitting-exercise/src/components/Loader/Loader.js
+++
@@ -1,9 +0,0 @@
-import './Loader.css';
-export const Loader = ({ hasMessage }) => {
-	return(
-		<div className="Loader">
-			<div className="Loader-picto"/>
-			{ hasMessage && <span>Loading...</span> }
-		</div>
-	)
-}
